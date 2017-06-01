ff40e15cf70707a86dc8fd8a1f0992a64ff9595a
Lindsay Carr <lcarr@usgs.gov>
2017-06-01 15:18:21 -0500

    use [[ not $

--- R/calc_water_year.R
+++ R/calc_water_year.R
@@ -1,8 +1,8 @@
 calc_water_year <- function(date.vec){
   # POSIXlt years start at 100, POSIXlt months start at 0
   date.time.vec <- as.POSIXlt(date.vec)
-  cal.year <- date.time.vec$year + 1900
-  cal.mon <- date.time.vec$mon + 1
+  cal.year <- date.time.vec[['year']] + 1900
+  cal.mon <- date.time.vec[['mon']] + 1
   
   # when the date is NA, it should not try to add 1
   which.past.oct <- cal.mon >= 10
