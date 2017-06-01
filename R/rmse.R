25f482fd1bb252ed749fcad146be7d9ada4ef2e5
Lindsay Carr <lcarr@usgs.gov>
2017-06-01 10:30:07 -0500

    named error but actually squared error

--- R/rmse.R
+++ R/rmse.R
@@ -1,6 +1,6 @@
 rmse <- function(obs, pred){
   #calculate root-mean-squared-error
-  error <- (obs - pred)^2
-  rmse <- sqrt(mean(error))
+  sqr.error <- (obs - pred)^2
+  rmse <- sqrt(mean(sqr.error))
   return(rmse)
 }
