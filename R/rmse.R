556d7324d84f9f7dd1ae8ecc26e36535a19c7396
Lindsay Carr <lcarr@usgs.gov>
2017-06-01 12:39:35 -0500

    add rmseByYear + expand rmse

--- R/rmse.R
+++ R/rmse.R
@@ -1,6 +1,19 @@
 rmse <- function(obs, pred){
   #calculate root-mean-squared-error
-  sqr.error <- (obs - pred)^2
-  rmse <- sqrt(mean(sqr.error))
+  sqr_error <- (obs - pred)^2
+  mn_sqr_error <- mean(sqr_error)
+  rmse <- sqrt(mn_sqr_error)
   return(rmse)
+}
+
+rmseByYear <- function(df){
+  
+  for(y in unique(df[['year']])){
+    df_y <- df[which(df[['year']] == y), ]
+    rmse_y <- rmse(df_y[['obs']], df_y[['pred']])
+    rmse_all <- c(rmse_all, rmse_y)
+    names(rmse_all)[length(rmse_all)] <- y
+  }
+  
+  return(rmse_all)
 }
